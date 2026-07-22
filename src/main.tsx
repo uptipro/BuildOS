3f6beb4275c09611fdfed8466cb0849ca613d114
Iniobong Ukpong <iniobong10000@gmail.com>
2026-07-22 18:13:20 +0100

    feat: implement Google Analytics integration and add environment variable example

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from "react-dom/client";
 import App from "./app/App";
 import "./styles/index.css";
+import { initGoogleAnalytics } from "./lib/googleAnalytics";
+
+initGoogleAnalytics();
 
 createRoot(document.getElementById("root")!).render(<App />);
